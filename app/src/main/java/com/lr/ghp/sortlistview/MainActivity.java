0424a43d75978955ba1e377bda18980df3e611a9
huiping.guo <huiping.guo@jimubox.com>
2015-07-10 15:27:28 +0800

    sortListView基本使用

---
+++ app/src/main/java/com/lr/ghp/sortlistview/MainActivity.java
@@ -0,0 +1,251 @@
+package com.lr.ghp.sortlistview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lr.ghp.model.GroupMemberBean;
+import com.lr.ghp.sortlistview.com.lr.ghp.adapter.SortGroupMemberAdapter;
+import com.lr.ghp.utility.CharacterParser;
+import com.lr.ghp.utility.PinyinComparator;
+import com.lr.ghp.view.SideBar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity implements SectionIndexer {
+
+    private ListView sortListView;
+    private SideBar sideBar;
+    private TextView dialog;
+    private SortGroupMemberAdapter adapter;
+    private EditText mClearEditText;
+
+    private LinearLayout titleLayout;
+    private TextView title;
+    private TextView tvNofriends;
+
+    private int lastFirstVisibleItem = -1;
+
+    private CharacterParser characterParser;
+    private List<GroupMemberBean> SourceDateList;
+
+    private PinyinComparator pinyinComparator;
+    private List<GroupMemberBean> filterDateList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        titleLayout = (LinearLayout) findViewById(R.id.title_layout);
+        title = (TextView) this.findViewById(R.id.title_layout_catalog);
+        tvNofriends = (TextView) this
+                .findViewById(R.id.title_layout_no_friends);
+
+        characterParser = CharacterParser.getInstance();
+
+        pinyinComparator = new PinyinComparator();
+
+        sideBar = (SideBar) findViewById(R.id.sidrbar);
+        dialog = (TextView) findViewById(R.id.dialog);
+//        sideBar.setTextView(dialog);
+
+        sideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
+
+            @Override
+            public void onTouchingLetterChanged(String s) {
+                int position = adapter.getPositionForSection(s.charAt(0));
+                if (position != -1) {
+                    sortListView.setSelection(position);
+                }
+            }
+        });
+
+        sortListView = (ListView) findViewById(R.id.country_lvcountry);
+        sortListView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(
+                        getApplication(),
+                        ((GroupMemberBean) adapter.getItem(position)).getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SourceDateList = filledData(getResources().getStringArray(R.array.date));
+
+        Collections.sort(SourceDateList, pinyinComparator);
+        adapter = new SortGroupMemberAdapter(this, SourceDateList);
+        sortListView.setAdapter(adapter);
+        sortListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+                int section = getSectionForPosition(firstVisibleItem);
+                int nextSection = getSectionForPosition(firstVisibleItem + 1);
+                int nextSecPosition = getPositionForSection(+nextSection);
+                if (firstVisibleItem != lastFirstVisibleItem) {
+                    MarginLayoutParams params = (MarginLayoutParams) titleLayout
+                            .getLayoutParams();
+                    params.topMargin = 0;
+                    titleLayout.setLayoutParams(params);
+                    title.setText(SourceDateList.get(
+                            getPositionForSection(section)).getSortLetters());
+                }
+                if (nextSecPosition == firstVisibleItem + 1) {
+                    View childView = view.getChildAt(0);
+                    if (childView != null) {
+                        int titleHeight = titleLayout.getHeight();
+                        int bottom = childView.getBottom();
+                        MarginLayoutParams params = (MarginLayoutParams) titleLayout
+                                .getLayoutParams();
+                        if (bottom < titleHeight) {
+                            float pushedDistance = bottom - titleHeight;
+                            params.topMargin = (int) pushedDistance;
+                            titleLayout.setLayoutParams(params);
+                        } else {
+                            if (params.topMargin != 0) {
+                                params.topMargin = 0;
+                                titleLayout.setLayoutParams(params);
+                            }
+                        }
+                    }
+                }
+                lastFirstVisibleItem = firstVisibleItem;
+                sideBar.setChoose(sideBar.getBIndex(section));
+                sideBar.invalidate();
+            }
+        });
+        mClearEditText = (EditText) findViewById(R.id.filter_edit);
+
+        mClearEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                titleLayout.setVisibility(View.GONE);
+                filterData(s.toString());
+
+//                if(!s.toString().equals("")){
+//                    int section=filterDateList.get(0).getSortLetters().charAt(0);
+//                    sideBar.setChoose(sideBar.getBIndex(section));
+//                }else{
+//                    sideBar.setChoose(0);
+//                }
+//                sideBar.invalidate();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private List<GroupMemberBean> filledData(String[] date) {
+        List<GroupMemberBean> mSortList = new ArrayList<GroupMemberBean>();
+
+        for (int i = 0; i < date.length; i++) {
+            GroupMemberBean sortModel = new GroupMemberBean();
+            sortModel.setName(date[i]);
+            String pinyin = characterParser.getSelling(date[i]);
+            String sortString = pinyin.substring(0, 1).toUpperCase();
+
+            if (sortString.matches("[A-Z]")) {
+                sortModel.setSortLetters(sortString.toUpperCase());
+            } else {
+                sortModel.setSortLetters("#");
+            }
+
+            mSortList.add(sortModel);
+        }
+        return mSortList;
+
+    }
+
+    private void filterData(String filterStr) {
+        filterDateList = new ArrayList<GroupMemberBean>();
+
+        if (TextUtils.isEmpty(filterStr)) {
+            filterDateList = SourceDateList;
+            tvNofriends.setVisibility(View.GONE);
+        } else {
+            filterDateList.clear();
+            for (GroupMemberBean sortModel : SourceDateList) {
+                String name = sortModel.getName();
+                if (name.indexOf(filterStr.toString()) != -1
+                        || characterParser.getSelling(name).startsWith(
+                        filterStr.toString())) {
+                    filterDateList.add(sortModel);
+                }
+            }
+        }
+
+        Collections.sort(filterDateList, pinyinComparator);
+        adapter.updateListView(filterDateList);
+        if (filterDateList.size() == 0) {
+            tvNofriends.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public Object[] getSections() {
+        return null;
+    }
+
+    /**
+     * 根据list的index获取首字母
+     * @param position
+     * @return
+     */
+    public int getSectionForPosition(int position) {
+        return SourceDateList.get(position).getSortLetters().charAt(0);
+    }
+
+    /**
+     * 根据首字母获取list对应的第一个的index
+     * @param section
+     * @return
+     */
+    public int getPositionForSection(int section) {
+        for (int i = 0; i < SourceDateList.size(); i++) {
+            String sortStr = SourceDateList.get(i).getSortLetters();
+            char firstChar = sortStr.toUpperCase().charAt(0);
+            if (firstChar == section) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ app/src/main/java/com/lr/ghp/utility/PinyinComparator.java
@@ -0,0 +1,22 @@
+package com.lr.ghp.utility;
+
+import com.lr.ghp.model.GroupMemberBean;
+
+import java.util.Comparator;
+
+
+public class PinyinComparator implements Comparator<GroupMemberBean> {
+
+	public int compare(GroupMemberBean o1, GroupMemberBean o2) {
+		if (o1.getSortLetters().equals("@")
+				|| o2.getSortLetters().equals("#")) {
+			return -1;
+		} else if (o1.getSortLetters().equals("#")
+				|| o2.getSortLetters().equals("@")) {
+			return 1;
+		} else {
+			return o1.getSortLetters().compareTo(o2.getSortLetters());
+		}
+	}
+
+}
